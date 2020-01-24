51214c0488e40858db96fa0c3c2c7ac152ad040c
vantonov <v.antonov@sigma-it.ru>
2020-01-24 12:13:45 +0300

    equity list formatting

--- web/src/components/common/equities-list.js
+++ web/src/components/common/equities-list.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import InfiniteScroll from 'react-infinite-scroll-component';
+import {Box} from "@material-ui/core";
+import ListItemText from "@material-ui/core/ListItemText";
 
 export default function EquitiesList(props) {
     return (<InfiniteScroll className="equities-list"
@@ -23,17 +23,16 @@ export default function EquitiesList(props) {
 }
 
 function Equity(props) {
+    const subway = props.address.subway;
+    const district = props.address.district;
     return <ListItem className="equities-item">
-        <ListItemText secondary={props.info}>
-            <ListItemSecondaryAction>
-                <div>{props.price}&#8381;</div>
-            </ListItemSecondaryAction>
-            {formatAddress(props.address)}
+        <ListItemText>
+            <Box width={1}><b>{props.price}&#8381;</b>&nbsp;{props.address.street}&nbsp;{props.address.building}</Box>
+            <Box width={1} className="MuiTypography-colorTextSecondary MuiTypography-body2">
+                {subway && <span><span style={{color: '#' + subway.color}}>&#9632;</span><span>&nbsp;{subway.name}&nbsp;</span></span>}
+                {!subway && district && <span>{district.name}&nbsp;р-н</span>}
+                <Box width={1}>{props.info}</Box>
+            </Box>
         </ListItemText>
     </ListItem>
-}
-
-function formatAddress(address) {
-    return (<div>{address.street}&nbsp;{address.building}</div>)
-
 }
