6274f0d797db732a36b3c1eb14cc50ab7b915d20
vantonov <v.antonov@sigma-it.ru>
2020-01-31 12:11:06 +0300

    show error message

--- web/src/api/fetch.js
+++ web/src/api/fetch.js
@@ -1,16 +1,21 @@
 export function fetchJSON(url, options) {
-    return fetch(url, options).then((response) => {
-        if (!response.ok) {
-            throw new Error(response.status + ' ' + response.statusText);
-        }
-        return response.json();
-    });
+    return fetch(url, options).then(response => handleResponse(response));
 }
 
 export function fetchEmpty(url, options) {
-    return fetch(url, options).then((response) => {
+    return fetch(url, options).then(response => {
         if (!response.ok) {
             throw new Error(response.status + ' ' + response.statusText);
         }
     });
+}
+
+function handleResponse(response) {
+    return response.json().then(json => {
+        if (response.ok) {
+            return json;
+        } else {
+            return Promise.reject(json)
+        }
+    })
 }
