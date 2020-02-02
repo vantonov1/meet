c0812162ad91a5128d1c13b555db4035173bcf79
vantonov <v.antonov@sigma-it.ru>
2020-02-02 08:47:55 +0300

    style equity

--- web/src/components/common/equities-list.js
+++ web/src/components/common/equities-list.js
@@ -5,6 +5,12 @@ import AutoSizer from "react-virtualized-auto-sizer";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Box from "@material-ui/core/Box";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles(theme => ({
+    squareMeters: { verticalAlign: "super"},
+    info: {whiteSpace: "nowrap", overflow: "hidden", textOverflow:"ellipsis"}
+}));
 
 export default function EquitiesList(props) {
     const [selectedItem, setSelectedItem] = React.useState(-1);
@@ -12,7 +18,6 @@ export default function EquitiesList(props) {
         <AutoSizer>
             {({height, width}) => (
                 <InfiniteScroll style={{width: width}}
-                                className="equities-list"
                                 dataLength={props.equities.length}
                                 next={props.onFetch}
                                 hasMore={props.hasMore}
@@ -36,23 +41,19 @@ export default function EquitiesList(props) {
 }
 
 function Equity(props) {
+    const classes = useStyles();
     const subway = props.address.subway;
     const district = props.address.district;
-    return <ListItem className="equities-item"
-                     button
-                     divider
-                     selected={props.selected}
-                     onClick={props.onClick}
-    >
+    return <ListItem button divider selected={props.selected} onClick={props.onClick}>
         <ListItemText>
             <Box width={1}><b>{props.price}&#8381;</b>&nbsp;{props.address.street}&nbsp;{props.address.building}</Box>
             <Box width={1} className="MuiTypography-colorTextSecondary MuiTypography-body2">
                 {subway && <span><span
                     style={{color: '#' + subway.color}}>&#9632;</span><span>&nbsp;{subway.name}&nbsp;</span></span>}
                 {!subway && district && <span>{district.name}&nbsp;р-н</span>}
-                <span>&nbsp;{props.square}м<span style={{ verticalAlign: "super"}}>2</span>
+                <span>&nbsp;{props.square}м<span className={classes.squareMeters}>2</span>
                     {props.rooms && <span>&nbsp;{props.rooms}кк</span>}</span>
-                <Box width="100%" style={{whiteSpace: "nowrap", overflow: "hidden", textOverflow:"ellipsis"}}>{props.info}</Box>
+                <Box width="100%" className={classes.info}>{props.info}</Box>
             </Box>
         </ListItemText>
     </ListItem>
