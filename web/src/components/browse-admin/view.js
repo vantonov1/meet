6fd1c0f28fbdc4d2edea0bd6b85e27d3a37ace92
vantonov <v.antonov@sigma-it.ru>
2020-02-14 08:25:14 +0300

    no restart on admin tabs

--- web/src/components/browse-admin/view.js
+++ web/src/components/browse-admin/view.js
@@ -4,6 +4,7 @@ import Tabs from "@material-ui/core/Tabs";
 import BrowseAdministrators from "../browse-admins/view";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import BrowseAgents from "../browse-agents/view";
+import {Link} from "react-router-dom";
 
 const TABS = [{url: 'agents', label: 'Агенты'}, {url: 'administrators', label: 'Администраторы'}];
 
@@ -19,11 +20,8 @@ export default function BrowseAdmin(props) {
     const classes = useStyles();
 
     return <>
-        <Tabs value={parseInt(tab)}
-              className={classes.root}
-              onChange={(e, v) => window.location.pathname = '/admin/' + TABS[v].url}
-        >
-            {TABS.map(t => <Tab key={t.url} label={t.label}/>)}
+        <Tabs value={parseInt(tab)} className={classes.root}>
+            {TABS.map(t => <Tab key={t.url} label={t.label} component={Link} to={t.url}/>)}
         </Tabs>
         {tab === '0' && <BrowseAgents/>}
         {tab === '1' && <BrowseAdministrators/>}
