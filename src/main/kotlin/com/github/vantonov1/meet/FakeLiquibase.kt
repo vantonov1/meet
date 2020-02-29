1d318b38bcd3501b8304ad5aea8ed03da1ea6943
vantonov <v.antonov@sigma-it.ru>
2020-02-29 06:57:48 +0300

    return liquibase; db drivers per profile

--- src/main/kotlin/com/github/vantonov1/meet/FakeLiquibase.kt
+++
@@ -1,7 +0,0 @@
-package com.github.vantonov1.meet
-
-import org.springframework.stereotype.Component
-
-@Component("liquibase")
-//@ConditionalOnMissingBean(name = ["liquibase"])
-class FakeLiquibase {}
